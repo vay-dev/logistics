26dca8d51290db3d0a3b2d130e6a74a7f7c3d63a
Viccodes <ayeniv69@gmail.com>
2025-07-28 20:54:07 +0100

    finished the guest page

--- src/guest/navbar.jsx
+++ src/guest/navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const GuestBar = () => {
   const navLinks = [
-    { label: "Home", href: "/home" },
+    { label: "Home", href: "/" },
     { label: "Services", href: "/services" },
     { label: "Track Order", href: "/track" },
     { label: "About Us", href: "/about" },
